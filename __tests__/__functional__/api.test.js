fbdb783ef3fa15976bcd8d5e21b2b95849c4f909
VladVes <veselov.workflow@gmail.com>
2018-02-09 11:29:24 +0700

    tests

---
+++ __tests__/__functional__/api.test.js
@@ -0,0 +1,38 @@
+import request from 'supertest';
+import matchers from 'jest-supertest-matchers';
+import app from '../../src/server';
+
+describe('api base', () => {
+  let server;
+
+  beforeAll(() => {
+    jasmine.addMatchers(matchers);
+  });
+
+  beforeEach(() => {
+    server = app().listen();
+  });
+
+  it('GET 200', async () => {
+    let res;
+    res = await request.agent(server).get('/');
+    expect(res).toHaveHTTPStatus(200);
+  });
+
+  /*
+  it('GET 302', async () => {
+    const res = await request.agent(server).get('/user/profile');
+    expect(res).toHaveHTTPStatus(302);
+  });
+  */
+
+  it('GET 404', async () => {
+    const res = await request.agent(server).get('/wrong-path');
+    expect(res).toHaveHTTPStatus(404);
+  });
+
+  afterEach((done) => {
+    server.close();
+    done();
+  });
+});
