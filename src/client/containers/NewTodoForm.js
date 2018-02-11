ce4b300b57c5c331f62bcaa7a4479add59ba652d
VladVes <veselov.workflow@gmail.com>
2018-02-11 23:04:15 +0700

    add containers

---
+++ src/client/containers/NewTodoForm.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux'; // eslint-disable-line
+import Component from '../components/NewToDoForm.jsx';
+import * as actionCreators from '../actions';
+
+const mapStateToProps = ({ taskCreatingState }) => {
+  const props = {
+    taskCreatingState,
+  };
+  return props;
+};
+
+const Container = connect(
+  mapStateToProps,
+  actionCreators,
+)(Component);
+
+export default Container;
