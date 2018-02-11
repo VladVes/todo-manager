96f6df390c514cd4b91163e19eaf1f21ff7442d2
VladVes <veselov.workflow@gmail.com>
2018-02-11 17:56:48 +0700

    add task model

--- src/server/models/tasks.js
+++ src/server/models/tasks.js
@@ -1,18 +1,53 @@
-const calculate = () => {
-
-};
 export default (mongoose) => {
   const taskSchema = mongoose.Schema({
-    header: String,
-    priority: String,
-    status: String,
-    deadLine: calculate();
-
+    header: {
+      type: String,
+      validate: {
+        validator(value) {
+          return value.length < 50;
+        },
+        message: `ToDo can be only 50 characters length`,
+      },
+      required: [true, 'task should have a content'],
+    },
+    priority: {
+      type: String,
+      validate: {
+        validator(value) {
+          return /hight|middle|low/i.test(value);
+        },
+        message: `${value} is not a valid priority`,
+      },
+      default: 'Low',
+      required: [true, 'task should have a priority'],
+    },
+    status: {
+      type: String,
+      validate: {
+        validator(value) {
+          return /new|active|resolved|closed/i.test(value);
+        },
+        message: `${value} is not a valid status`,
+      },
+      default: 'New',
+      required: [true, 'task should have a status'],
+    },
+    deadLine: {
+      type: String,
+      validate: {
+        validator(value) {
+          return true; ///UPDATE validator!!!
+        },
+        message: `Deadline can't be today`,
+      },
+      required: [true, 'task should have a deadline'],
     }
+
   });
-  taskSchema.methods.show = function() {
-    return `Task is ${this.name} and it's about ${this.description}`;
+
+  taskSchema.methods.toString = function() {
+    return `Task is: ${this.header}`;
   };
 
-  const Task = mongoose.model('Task', taskSchema);
-}
+  return mongoose.model('Task', taskSchema);
+};
