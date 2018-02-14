2aa1dfbf71e6724a23d00b2cc137d43beed50901
VladVes <veselov.workflow@gmail.com>
2018-02-14 17:01:27 +0700

    add queue path

--- src/client/routes.js
+++ src/client/routes.js
@@ -3,4 +3,5 @@ const host = '';
 export default {
   tasksUrl: () => [host, 'tasks'].join('/'),
   taskUrl: id => [host, 'tasks', id].join('/'),
+  changeTaskOrderUrl: id => [host, 'tasks', id, 'ordering'].join('/'),
 };
