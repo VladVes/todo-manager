99c5f87f4ae9cb20917d1a37ff9586f5d2f75001
VladVes <veselov.workflow@gmail.com>
2018-02-15 22:51:25 +0700

    add date field validation

--- src/client/components/validators.js
+++ src/client/components/validators.js
@@ -6,22 +6,8 @@ const maxLength = max => value => (
 );
 export const maxLength50 = maxLength(50);
 
-export const normalizeDate = (value) => {
-  if (!value) {
-    return value;
-  }
-  const onlyNums = value.replace(/[^\d]/g, '');
-
-  if (onlyNums.length <= 2 && /^[0-3]$/.test(onlyNums)) {
-    return onlyNums;
-  }
-  /*
-  if (onlyNums[0] === 1 && /^[0-3]?$/.test(onlyNums)) {
-    return onlyNums;
-  }
-  if (onlyNums.length <= 4 && /^[0-3][0-9][0-1][0-9]?$/.test(onlyNums)) {
-    return `${onlyNums.slice(0, 2)}.${onlyNums.slice(2)}`;
-  }
-  return `${onlyNums.slice(0, 3)}-${onlyNums.slice(3, 6)}-${onlyNums.slice(6, 10)}`;
-  */
+export const laterThenToday = (value) => {
+  const now = new Date();
+  const recived = new Date(value);
+  return now.getTime() >= recived.getTime() ? 'Deadline should be later than today' : undefined;
 };
