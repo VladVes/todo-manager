d7ed4ef517e1e06aa1c72f62075a58f40a8d1460
VladVes <veselov.workflow@gmail.com>
2018-02-17 21:00:30 +0700

    add task update action

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -18,6 +18,10 @@ export const addTaskRequest = createAction('TASK_ADD_REQUEST');
 export const addTaskSuccess = createAction('TASK_ADD_SUCCESS');
 export const addTaskFailure = createAction('TASK_ADD_FAILURE');
 
+export const updateTaskRequest = createAction('TASK_UPDATE_REQUEST');
+export const updateTaskSuccess = createAction('TASK_UPDATE_SUCCESS');
+export const updateTaskFailure = createAction('TASK_UPDATE_FAILURE');
+
 export const taskOrderingRequest = createAction('TASK_ORDER_REQUEST');
 export const taskOrderingSuccess = createAction('TASK_ORDER_SUCCESS');
 export const taskOrderingFailure = createAction('TASK_ORDER_FAILURE');
@@ -29,6 +33,27 @@ export const addTask = task => async (dispatch) => {
     dispatch(addTaskSuccess({ data: response.data }));
   } catch (e) {
     dispatch(addTaskFailure());
+  }
+};
+
+export const updateTask = task => async (dispatch) => {
+  dispatch(updateTaskRequest());
+  const {
+    header,
+    priority,
+    status,
+    deadline,
+  } = task;
+  try {
+    const response = await axios.patch(routes.taskUrl(task._id), {  // eslint-disable-line
+      header,
+      priority,
+      status,
+      deadline,
+    });
+    dispatch(updateTaskSuccess({ data: response.data }));
+  } catch (e) {
+    dispatch(updateTaskFailure());
   }
 };
 
@@ -50,19 +75,17 @@ export const removeTask = task => async (dispatch) => {
     const responde = await axios.delete(url);
     dispatch(removeTaskSuccess({ task, data: responde.data }));
   } catch (e) {
-    console.log(e);
     dispatch(removeTaskFailure({ task }));
   }
 };
 
-export const changeTaskOrder = (order) => async (dispatch) => {
+export const changeTaskOrder = order => async (dispatch) => {
   dispatch(taskOrderingRequest());
   try {
     const url = routes.changeTaskOrderUrl(order.taskId);
     const response = await axios.post(url, { order });
     dispatch(taskOrderingSuccess({ data: response.data }));
   } catch (e) {
-    console.log(e);
-    dispatch(taskOrderingFailure());
+    dispatch(taskOrderingFailure(e));
   }
 };
