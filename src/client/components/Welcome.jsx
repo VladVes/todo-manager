ddf89ab4488fe27222ade51d776b27954b4f43d6
VladVes <veselov.workflow@gmail.com>
2018-02-18 01:47:02 +0700

    fix eslint errors

--- src/client/components/Welcome.jsx
+++ src/client/components/Welcome.jsx
@@ -1,16 +1,13 @@
 import React from 'react'; // eslint-disable-line
 import { NavLink } from 'react-router-dom'; // eslint-disable-line
 
-const Welcome = (props) => {
-  return (
-    <div>
-      <h1>Welcome to the ToDo manager</h1>
-      <p>Drive your tasks and get things done.</p>
-        <NavLink to='tasks'>
-          <button className="btn btn-primary btn-lg">PROCEED</button>
-        </NavLink>
-    </div>
-  );
-};
+const Welcome = () =>
+  <div>
+    <h1>Welcome to the ToDo manager</h1>
+    <p>Drive your tasks and get things done.</p>
+      <NavLink to='tasks'>
+        <button className="btn btn-primary btn-lg">PROCEED</button>
+      </NavLink>
+  </div>;
 
 export default Welcome;
