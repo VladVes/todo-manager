e2c5018f6fe93ce240e172db86e328169231d5ad
VladVes <veselov.workflow@gmail.com>
2018-02-18 01:47:51 +0700

    update eslint disable

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/main.sass';
 
 import React from 'react' // eslint-disable-line
 import { render } from 'react-dom' // eslint-disable-line
 import thunk from 'redux-thunk'; // eslint-disable-line
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux'; // eslint-disable-line
 import reducers from './reducers';
 import App from './components/App.jsx'; // eslint-disable-line
 import { fetchTasks } from './actions';
 
 /* eslint-disable no-underscore-dangle */
-const ext = window.__REDUX_DEVTOOLS_EXTENSION__;
+const ext = window.__REDUX_DEVTOOLS_EXTENSION__; // eslint-disable-line
 const devtoolMiddleware = ext && ext();
 /* eslint-enable */
 
@@ -24,5 +25,5 @@ store.dispatch(fetchTasks());
 
 render(
   <App store={store} />,
-  document.getElementById('root')
+  document.getElementById('root') // eslint-disable-line
 );
